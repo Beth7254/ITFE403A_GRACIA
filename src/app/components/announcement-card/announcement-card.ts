508aa240b64fef3aab95e019e932539fc4f442b4
Beth7254 <marybeth.gracia@dbtc-cebu.edu.ph>
2026-08-05 10:38:01 +0800

    Feat: Interpolation Activity

--- src/app/components/announcement-card/announcement-card.ts
+++ src/app/components/announcement-card/announcement-card.ts
@@ -2,9 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-announcement-card',
-  imports: [],
   templateUrl: './announcement-card.html',
   styleUrl: './announcement-card.css'
 })
+
 export class AnnouncementCard {
+  title1 = 'Student Portal Launch';
+  body1 = 'Students can access important school information, announcements, and student services.';
+
+  title2 = 'SSS';
+  body2 = 'Important information about the SSS program.';
+
+  title3 = 'Scholarship Opportunities';
+  body3 = 'Information about available scholarships for students.';
+
+  title4 = 'Upcoming Events';
+  body4 = 'Details about upcoming events and activities organized by the school.';
+
+  title5 = 'Campus Safety';
+  body5 = 'Guidelines and resources for ensuring the safety and security of students on campus.';
+
+  title6 = 'Student Support Services';
+  body6 = 'Information about counseling, health services, and other support resources available to students.';
+
+  title7 = 'Academic Calendar';
+  body7 = 'Important dates and deadlines for the academic year, including registration, exams, and holidays.';
+
+  title8 = 'Library Resources'; 
+  body8 = 'Access to online databases, e-books, and other library resources for research and study purposes.';
+
+  title9 = 'Career Development';  
+  body9 = 'Information about career counseling, job fairs, and internship opportunities for students.';
+
+  title10 = 'Student Organizations';   
+  body10 = 'Details about student clubs, organizations, and extracurricular activities available on campus.';
 }
